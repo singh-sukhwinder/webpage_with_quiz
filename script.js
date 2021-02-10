4688004c57dc61ee0989181a7326b6e5b484efa0
saipreethi14 <74370444+saipreethi14@users.noreply.github.com>
2021-02-10 08:14:20 +0530

    Update script.js
    updation

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
       // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
+      // to move the page scroll smoothly jQueries "animate" method
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
